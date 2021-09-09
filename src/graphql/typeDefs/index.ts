087e1911fc027b03fe0f2950d12c8aaa990dea74
Caio Tony <caiotony16@gmail.com>
2021-09-09 19:20:36 +0100

    feat: add user graphql mutation

--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -2,6 +2,9 @@ import { gql } from 'apollo-server';
 import { DocumentNode } from 'graphql';
 
 export const typeDefs: DocumentNode = gql`
+	type Mutation {
+		addUser(username: String, password: String): user
+	}
 	type user {
 		id: String
 		username: String
